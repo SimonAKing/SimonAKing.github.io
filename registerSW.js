d692ecf6847adc3df6da406f596ed447344b9422
Tomotoes <simon@tomotoes.com>
2021-03-31 23:28:51 +0800

    update at Wed Mar 31 2021

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-var SWVersion="5.203.3";"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register(`/sw.js?v=${SWVersion}`).catch(function(e){console.log("ServiceWorker registration failed: ",e)})});
+var SWVersion="5.204.3";"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register(`/sw.js?v=${SWVersion}`).catch(function(e){console.log("ServiceWorker registration failed: ",e)})});
