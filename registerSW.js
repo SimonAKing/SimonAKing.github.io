496928f5a936b1e7deb8398c138776f62ef81691
SimonAKing <hi@simonaking.com>
2025-05-18 14:09:22 +0800

    update at Sun May 18 2025

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-var SWVersion="5.308.3";"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register(`/sw.js?v=${SWVersion}`).catch(function(e){console.log("ServiceWorker registration failed: ",e)})});
+var SWVersion="5.309.3";"serviceWorker"in navigator&&window.addEventListener("load",function(){navigator.serviceWorker.register(`/sw.js?v=${SWVersion}`).catch(function(e){console.log("ServiceWorker registration failed: ",e)})});
